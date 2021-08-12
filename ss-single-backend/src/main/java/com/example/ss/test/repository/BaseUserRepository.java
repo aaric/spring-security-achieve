6f05738cd5afe5c44ed085aedc028b06377d8119
qlhan <qlhan@incarcloud.com>
2021-08-12 21:51:56 +0800

    0.5.0-SNAPSHOT add db auth

--- ss-single-backend/src/main/java/com/example/ss/test/repository/BaseUserRepository.java
+++ ss-single-backend/src/main/java/com/example/ss/test/repository/BaseUserRepository.java
@@ -2,10 +2,13 @@ package com.example.ss.test.repository;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.ss.pojo.BaseUser;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
- * hello title
+ * 用户信息DAO接口
  *
  * @author Aaric, created on 2021-08-11T16:25.
  * @version 0.4.0-SNAPSHOT
@@ -14,11 +17,10 @@ import org.springframework.stereotype.Repository;
 public interface BaseUserRepository extends BaseMapper<BaseUser> {
 
     /**
-     * 获取记录总数
+     * 查询用户拥有权限列表
      *
+     * @param id 用户ID
      * @return
      */
-    Long totalRecord();
-
-    //List<String>
+    List<String> selectAuthorityList(@Param("id") Long id);
 }
