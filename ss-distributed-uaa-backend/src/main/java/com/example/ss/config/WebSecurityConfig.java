90913a595d20a474f546f86d594415c9d0b11a55
qlhan <qlhan@incarcloud.com>
2021-08-14 08:34:15 +0800

    0.7.0-SNAPSHOT /oauth/token

--- ss-distributed-uaa-backend/src/main/java/com/example/ss/config/WebSecurityConfig.java
+++ ss-distributed-uaa-backend/src/main/java/com/example/ss/config/WebSecurityConfig.java
@@ -42,6 +42,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/doc.html", "/webjars/**", "/swagger-resources", "/v2/api-docs").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .formLogin()
+                .and()
                 .csrf().disable();
     }
 }
