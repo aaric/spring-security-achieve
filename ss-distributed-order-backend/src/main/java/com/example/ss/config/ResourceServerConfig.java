b1ba662de074bb3b7a2d9db4771ea637aeba588b
qlhan <qlhan@incarcloud.com>
2021-08-18 11:40:21 +0800

    0.8.0-SNAPSHOT fixed run

--- ss-distributed-order-backend/src/main/java/com/example/ss/config/ResourceServerConfig.java
+++ ss-distributed-order-backend/src/main/java/com/example/ss/config/ResourceServerConfig.java
@@ -2,7 +2,6 @@ package com.example.ss.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -18,7 +17,6 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
  */
 @Configuration
 @EnableResourceServer
-@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     public static final String RESOURCE_ID = "res1";
